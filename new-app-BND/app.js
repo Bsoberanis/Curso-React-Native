9d74a97350e79492fca39a7498131af82180b0db
Bsoberanis <bsoberanis-20233433@kinal.edu.gt>
2025-06-20 16:44:59 -0600

    todo app 1

--- new-app-BND/app.js
+++ new-app-BND/app.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet , text , view } from 'react-native';     
-import MainStackNavigator from './navigation/MainNavigator.js';
-
-
 
 export default function App() {
     return (
-       <MainStackNavigator/>
+        <view style={styles.container}>
+        <text>Open up App.js to start working on your app!</text>
+        <StatusBar style="auto" />
+        </view>
     );
 }
-
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
